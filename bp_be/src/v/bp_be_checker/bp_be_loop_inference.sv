4142a4f523ddbc5fe11a8ddb287218e187be2db4
BrendenPage <brendenpage@gmail.com>
2024-08-11 11:14:27 -0700

    remove counter set down in attempt to remove combinational loop

--- bp_be/src/v/bp_be_checker/bp_be_loop_inference.sv
+++ bp_be/src/v/bp_be_checker/bp_be_loop_inference.sv
@@ -91,16 +91,36 @@ module bp_be_loop_inference
   logic set;
 
   logic [`BSG_WIDTH(discovery_misses_p)-1:0] skips_remaining;
-  bsg_counter_set_down
-    #(.width_p(`BSG_WIDTH(discovery_misses_p)))
-    discovery_cooldown
-      (.clk_i(clk_i)
-      ,.reset_i(reset_i)
-      ,.set_i(set)
-      ,.val_i(discovery_misses_p[`BSG_WIDTH(discovery_misses_p)-1:0])
-      ,.down_i(confirm_discovery_i & state_r == 3'b010 & striding_pc_i != striding_pc_r)
-      ,.count_r_o(skips_remaining)
-      );
+  // bsg_counter_set_down
+  //   #(.width_p(`BSG_WIDTH(discovery_misses_p)))
+  //   discovery_cooldown
+  //     (.clk_i(clk_i)
+  //     ,.reset_i(reset_i)
+  //     ,.set_i(set)
+  //     ,.val_i(discovery_misses_p[`BSG_WIDTH(discovery_misses_p)-1:0])
+  //     ,.down_i(confirm_discovery_i & state_r == 3'b010 & striding_pc_i != striding_pc_r)
+  //     ,.count_r_o(skips_remaining)
+  //     );
+  
+  logic[`BSG_WIDTH(discovery_misses_p)-1: 0] ctr_r, ctr_n;
+  always_ff @(posedge clk_i) begin
+    if (reset_i) begin
+      ctr_r <= discovery_misses_p[`BSG_WIDTH(discovery_misses_p)-1:0];
+    end
+    else begin
+      ctr_r <= ctr_n;
+    end
+  end
+
+  always_comb begin
+	    ctr_n = ctr_r;
+	    if (set)
+        ctr_n = val_i;
+	    else if (confirm_discovery_i & state_r == 3'b010 & striding_pc_i != striding_pc_r)
+		    ctr_n = ctr_n - 1;
+  end
+
+  assign skips_remaining = ctr_r;
 
   // register difference calculations
 
